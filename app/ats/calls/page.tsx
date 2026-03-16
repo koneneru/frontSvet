e646120ec07abc78a4e5d2c33a5e98576286d44b
k0neru <koneneru@gmail.com>
2026-03-16 12:29:55 +0400

    feat(page): add templated title

--- app/ats/calls/page.tsx
+++ app/ats/calls/page.tsx
@@ -1,4 +1,9 @@
 import { AtsCallsPage } from '@/pages/atsCallPage';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Журнал вызовов АТС',
+};
 
 export default function Page() {
   return <AtsCallsPage />;
--- app/ats/gateways/page.tsx
+++ app/ats/gateways/page.tsx
@@ -1,4 +1,9 @@
 import { AtsGatewaysPage } from '@/pages/atsGatewaysPage';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Шлюзы АТС',
+};
 
 export default function Page() {
   return <AtsGatewaysPage />;
